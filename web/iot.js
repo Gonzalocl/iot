76a1ca9bee0137351e5a95e61cfca3578942c704
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-03-05 10:56:03 +0100

    request sensor data

--- web/iot.js
+++ web/iot.js
@@ -1,4 +1,11 @@
-document.getElementById("main_objet").innerHTML = "hola5"
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/data', true);
-xhr.send(null);
+document.getElementById('main_objet').innerHTML = 'Loading...';
+var data_request = new XMLHttpRequest();
+data_request.responseType = 'json';
+data_request.open('GET', '/data', true);
+data_request.onload = data_received;
+data_request.send(null);
+
+function data_received() {
+    var sensor_data = data_request.response;
+    console.log(sensor_data);
+}
