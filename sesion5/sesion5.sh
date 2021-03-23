d39a13fdfe2a3a698ee9d1d35fb5207635362d20
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-03-23 14:00:27 +0100

    simulated arduino

--- sesion5/sesion5.sh
+++ sesion5/sesion5.sh
@@ -1,5 +1,8 @@
 docker-compose up
 
+python simulated_arduino.py localhost 2
+python simulated_arduino.py localhost 3
+
 python query_lab.py localhost 1
 python query_lab.py localhost 2
 python query_lab.py localhost 3
--- sesion5/simulated_arduino.py
+++ sesion5/simulated_arduino.py
@@ -3,6 +3,45 @@ import argparse
 import requests
 import time
 
+template_url = 'http://{}:1026/v1/updateContext'
+template_payload = {
+    'contextElements': [
+        {
+            'type': 'Laboratorio',
+            'isPattern': 'false',
+            'id': '',
+            'attributes': [
+                {
+                    'name': 'temperature',
+                    'type': 'float',
+                    'value': 0
+                },
+                {
+                    'name': 'humidity',
+                    'type': 'float',
+                    'value': 0
+                }
+            ]
+        }
+    ],
+    'updateAction': ''
+}
+
+def ngsi_create(fiware_ip, fiware_lab, temperature, humidity):
+    return ngsi_send_request(fiware_ip, fiware_lab, temperature, humidity, 'APPEND')
+
+def ngsi_update(fiware_ip, fiware_lab, temperature, humidity):
+    return ngsi_send_request(fiware_ip, fiware_lab, temperature, humidity, 'UPDATE')
+
+def ngsi_send_request(fiware_ip, fiware_lab, temperature, humidity, updateAction):
+    url = template_url.format(fiware_ip)
+    template_payload['contextElements'][0]['id'] = 'Laboratorio{}'.format(fiware_lab)
+    template_payload['contextElements'][0]['attributes'][0]['value'] = temperature
+    template_payload['contextElements'][0]['attributes'][1]['value'] = humidity
+    template_payload['updateAction'] = updateAction
+    response = requests.post(url, json=template_payload).json()
+    return
+
 ip = ''
 lab = ''
 
@@ -10,11 +49,13 @@ def random_temperature_humidity():
     return 10, 15
 
 def setup():
-    pass
+    temperature, humidity = random_temperature_humidity()
+    ngsi_create(ip, lab, temperature, humidity)
 
 def loop():
-
     time.sleep(5)
+    temperature, humidity = random_temperature_humidity()
+    ngsi_update(ip, lab, temperature, humidity)
 
 def simulated_arduino(fiware_ip, fiware_lab):
     global ip, lab
