cdb8a1fe8dce670ac99808420eb9e2fc19244f31
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-03-23 13:02:07 +0100

    ip args

--- sesion5/simulated_arduino.py
+++ sesion5/simulated_arduino.py
@@ -1,6 +1,10 @@
 
+import argparse
 import requests
 import time
+
+ip = ''
+lab = ''
 
 def random_temperature_humidity():
     return 10, 15
@@ -12,7 +16,18 @@ def loop():
 
     time.sleep(5)
 
-if __name__ == '__main__':
+def simulated_arduino(fiware_ip, fiware_lab):
+    global ip, lab
+    ip = fiware_ip
+    lab = fiware_lab
+
     setup()
     while True:
         loop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip')
+    parser.add_argument('lab')
+    args = parser.parse_args()
+    simulated_arduino(args.ip, args.lab)
