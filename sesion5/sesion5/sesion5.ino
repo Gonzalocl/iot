2d0514b4e32cce936ef0b3f93dca9c2c4d4d9438
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-03-24 19:09:37 +0100

    basic temperature and humedity

---
+++ sesion5/sesion5/sesion5.ino
@@ -0,0 +1,49 @@
+#include<DHT.h>
+
+#define DHT_PIN 3
+
+DHT dht(DHT_PIN, DHT11);
+
+void setup() {
+  
+  Serial.begin(9600);
+  while(!Serial);
+
+  dht.begin();
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return;
+  }
+
+  print_sensor_info(temperature, humidity);
+
+}
+
+void print_sensor_info(float temperature, float humidity) {
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" ºC | ");
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+}
+
+void loop() {
+
+  delay(5000);
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return;
+  }
+
+  print_sensor_info(temperature, humidity);
+  
+}
