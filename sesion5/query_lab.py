7a95ebaf84446eecbe0dbc4f7cfe4dd739e1f44f
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-04-24 19:53:59 +0200

    spaces

--- sesion5/query_lab.py
+++ sesion5/query_lab.py
@@ -15,35 +15,45 @@ template_payload = {
 }
 
 def query_lab(ip, lab):
+    
     url = template_url.format(ip)
     template_payload['entities'][0]['id'] = 'Laboratorio{}'.format(lab)
     if lab == '*':
         template_payload['entities'][0]['isPattern'] = 'true'
+
     response = requests.post(url, json=template_payload).json()
+
     try:
         if not 'contextResponses' in response:
             return response, None
+
         data = []
         for r in response['contextResponses']:
             element_id = r['contextElement']['id']
             temperature = None
             humidity = None
+
             for a in r['contextElement']['attributes']:
                 if a['name'] == 'temperature':
                     temperature = a['value']
                 elif a['name'] == 'humidity':
                     humidity = a['value']
+
             data.append({'element_id': element_id, 'temperature': temperature, 'humidity': humidity})
+
         return response, data
+
     except KeyError:
         return response, None
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('ip')
     parser.add_argument('lab')
     parser.add_argument('--print-json', default=False, action='store_true')
     args = parser.parse_args()
+
     json, data = query_lab(args.ip, args.lab)
 
     if not data:
