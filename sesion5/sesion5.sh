9bf25a8a5714610cf51050bed6d349b83cab885e
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-04-25 19:12:42 +0200

    accumulator server command

--- sesion5/sesion5.sh
+++ sesion5/sesion5.sh
@@ -8,4 +8,6 @@ python query_lab.py localhost 2
 python query_lab.py localhost 3
 python query_lab.py localhost \*
 
+python2 accumulator-server.py --port 1111 --url /alerta --pretty-print -v
+
 docker container stop sesion5_orion_1 sesion5_mongo_1
