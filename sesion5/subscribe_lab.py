45e0e24d69c1d1afa16c4bfa1772f25bf8be5202
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-04-25 19:49:08 +0200

    subscribe lab

--- sesion5/subscribe_lab.py
+++ sesion5/subscribe_lab.py
@@ -1,6 +1,7 @@
 
 import argparse
 import requests
+from pprint import pprint
 
 template_url = 'http://{}:1026/v1/subscribeContext'
 template_payload = {
@@ -28,7 +29,22 @@ template_payload = {
 }
 
 def subscribe_lab(ip, lab):
-    pass
+
+    url = template_url.format(ip)
+    template_payload['entities'][0]['id'] = 'Laboratorio{}'.format(lab)
+    if lab == '*':
+        template_payload['entities'][0]['isPattern'] = 'true'
+
+    response = requests.post(url, json=template_payload).json()
+
+    try:
+        if not 'subscribeResponse' in response:
+            return response, None
+
+        return response, response['subscribeResponse']['subscriptionId']
+
+    except KeyError:
+        return response, None
 
 if __name__ == '__main__':
 
@@ -36,3 +52,12 @@ if __name__ == '__main__':
     parser.add_argument('ip')
     parser.add_argument('lab')
     args = parser.parse_args()
+
+    json, subscription_id = subscribe_lab(args.ip, args.lab)
+
+    if not subscription_id:
+        print('ERROR')
+        pprint(json)
+        exit(1)
+
+    print('subscriptionId: {}'.format(subscription_id))
