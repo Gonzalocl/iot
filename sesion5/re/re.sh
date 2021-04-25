a7b1be99e30227e6940d36427d7305ed463bb17e
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-04-26 00:34:14 +0200

    test

---
+++ sesion5/re/re.sh
@@ -0,0 +1,13 @@
+nc -lp 1026 > req
+python simulated_arduino.py localhost 2
+
+# start fiware
+nc localhost 1026 < req > rep
+# stop fiware
+
+for i in $(seq 0 9); do
+  echo "$i"
+  nc -lp 1026 < rep > "req$i"
+done
+
+rm -f rep req*
